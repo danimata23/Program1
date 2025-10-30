d24df63489510d3aa549742cf157924c63bed48f
danimata23 <dmata9@bruinmail.slcc.edu>
2025-10-29 18:27:54 -0600

    Initial commit - Program 1 uploaded

---
+++ Program1.py
@@ -0,0 +1,13 @@
+# Name: Daniela M
+# Assignment: A01
+
+#step1
+print ("@..@")
+print ("(----)")
+print ("( >__< )")
+print ("^^ ~~ ^^")
+
+#Step2
+state = input("US state: ")
+capital = input("Capital: ")
+print("The capital of " + state + " is " + capital)
